a5791f3ac57293f7ec6cff9c98b366731c411c08
AdrijusC <asciurinskas@gmail.com>
2026-02-28 16:56:25 +0200

    new

--- learning/script.js
+++ learning/script.js
@@ -1,6 +1,9 @@
 let name = "Adrijus"
 let age = 16;
 
+// name += "@"
+console.log(name);
+
 
 // age = age + 3
 // age += 3
@@ -102,3 +105,136 @@ console.log(sumArrayNum(numbers)) // 64
 
 
 
+
+let arr1 = [21, 65, 87, 43, 22]  // 324
+let arr2 = [87, 87, 43, 211]   // 543
+let arr3 = [87, 43, 22]       // 134
+let arr4 = [21, 65, 87, 43, 98, 99, 65]  // 676
+
+function addArray(arr){
+    let total = 0;
+    for (let i = 0; i < arr.length ; i++){
+        total += arr[i]
+    }
+    return total
+}
+
+console.log("add ");
+
+console.log(addArray(arr1))
+console.log(addArray(arr2))
+console.log(addArray(arr3))
+console.log(addArray(arr4))
+
+function avgArray(arr){
+    let total = addArray(arr)
+    return total / arr.length
+}
+
+console.log("avg");
+console.log(avgArray(arr1))
+console.log(avgArray(arr2))
+console.log(avgArray(arr3))
+console.log(avgArray(arr4))
+
+// fives = 0 + 1 + 1 
+// i =  5
+// [32, 5, 76 5, 12] 
+
+function fiveCounter(arr){
+    let fives = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === 5){
+            fives++
+        }
+    }
+    return fives
+}
+
+console.log(fiveCounter([32, 5, 76, 5, 12] ));
+console.log(fiveCounter([32, 15, 76, 25, 12] ));
+
+
+// xCounter
+
+console.log(xCounter([32, 5, 76, 5, 12] , 5));
+console.log(xCounter([32, 15, 76, 25, 12], 12 ));
+
+
+
+function xCounter(arr, number){
+    let total = 0;
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] === number){
+            total++
+        }
+    }
+    return total;
+}
+
+
+
+let strArr = [21, 676, 43, 12] 
+
+function returnString(arr){
+    let string = "";
+    for (let i = 0; i < arr.length; i++){
+        string += arr[i] + ";"
+    }
+    return string;
+}
+console.log(returnString(strArr))
+
+
+// [21, 76, 8, 65, 32]  [322, 6, 544]   "322;6;544;"
+
+function largestArr(arr1, arr2){
+    if ( addArray(arr1) >  addArray(arr2)){
+        return returnString(arr1)
+    }else{
+        return returnString(arr2)
+    }
+}
+
+
+let fiveArr = [21, 76, 5, 65, 32]
+// [21, 5, 76, 65, 32]  false
+
+function fiveChecker(arr){
+    for (let i = 0; i < arr.length; i++){
+        if(arr[i] === 5){
+            return true;
+        }
+    }
+    return false;
+}
+// function fiveChecker(arr){
+//     let isFiveThere = false;
+//     for (let i = 0; i < arr.length; i++){
+//         if(arr[i] === 5){
+//             isFiveThere = true;
+//         }
+//     }
+//     return isFiveThere;
+// }
+
+console.log(fiveChecker(fiveArr))
+
+function sumFirstHalf(arr){
+    let firstHalf = arr.length / 2;
+    let firstHalfTotal = 0;
+    for (let i = 0; i < firstHalf; i++){
+        firstHalfTotal += arr[i]
+    }
+    return firstHalfTotal;
+}
+function sumSecondHalf(arr){
+    let firstHalf = arr.length / 2;
+    let firstHalfTotal = 0;
+    for (let i = firstHalf; i < arr.length; i++){
+        firstHalfTotal += arr[i]
+    }
+    return firstHalfTotal;
+}
+
+console.log(sumSecondHalf(fiveArr))
