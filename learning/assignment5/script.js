8b44712f0989ec8a425d12d4fddf3aaa0b2d2c91
AdrijusC <263057910+Adrijus-code@users.noreply.github.com>
2026-04-01 20:09:18 +0300

    new

--- learning/assignment5/script.js
+++ learning/assignment5/script.js
@@ -7,6 +7,9 @@ const question_num = document.querySelector('.question-num')
 const question_p = document.querySelector('.question-p')
 const question_display_div = document.querySelector('#question-display-div')
 const answer_div = document.querySelector('.answer-div')
+const submit_btn = document.querySelector('.submit-btn')
+const score_display_h3 = document.querySelector('.score-display-h3')
+const tbody = document.querySelector("tbody")
 
 
 async function loadData(){
@@ -34,13 +37,48 @@ window.displayQuestion = function(nbr){
     `
     })
     answer_div.innerHTML = answers
-  
 }
 
 // user chose Washington as answer for question 1
 // user chose Earth as answer for question 2
 // user chose Au as answer for question 3
+submit_btn.addEventListener('click', checkAnswers)
 
+function checkAnswers(){
+    for(let answer of user_answers_arr){
+        if(!answer || user_answers_arr.length < all_questions.length){
+            alert("Must answer all questions first")
+            return
+        }
+    }
+    // console.log(all_questions)
+    let score = 0
+    let str = ""
+    const all_correct_answers = all_questions.map(question =>{
+        // return question.answer
+        return question.choices[question.answer]
+    })
+    
+    for (let i = 0; i < all_correct_answers.length; i++){
+        if(user_answers_arr[i] === all_correct_answers[i]){
+            score++
+        }
+        str += `
+                    <tr>
+                        <td>question ${i + 1}</td>
+                        <td>${all_questions[i].questionText}</td>
+                        <td>${all_correct_answers[i]}</td>
+                        <td>${user_answers_arr[i]}</td>
+                        <td>${user_answers_arr[i] === all_correct_answers[i] ? "1":"0"}</td>
+                    </tr>`
+    }
+    score_display_h3.innerHTML = `Your score is ${score}/${all_questions.length}`
+    tbody.innerHTML = str
+    // console.log(all_correct_answers);
+    // console.log(user_answers_arr);
+    
+    
+}
 
 // [ "Washing DC" , "earth", "frere",  "grtrh","thhty" , "reer", "feez"]
 window.radioClick = function(choice, i){
