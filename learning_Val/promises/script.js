66cde32468866d30c359e9d98ae753090fb80cf6
AdrijusC <263057910+Adrijus-code@users.noreply.github.com>
2026-05-23 22:35:47 +0300

    Completed hw task

--- learning_Val/promises/script.js
+++ learning_Val/promises/script.js
@@ -1,23 +1,50 @@
 const ul = document.querySelector("ul");
-console.time('test')
+
 console.log(ul);
-let array;
+
 fetch("https://jsonplaceholder.typicode.com/users")
   .then((payload) => payload.json())
-  .then((data) => displayUser(data));
+  .then((data) => {
+    displayUser(data)
+    const allUsers = document.querySelectorAll('.user-info')
+    allUsers.forEach(user=>{
+      user.addEventListener('click', getUserPosts)
+    })
+  });
 
 
 
 function displayUser(arr){
-  console.timeLog('test')
   for(let i = 0; i < arr.length; i++){
-    let user = document.createElement('li');
-    user.innerText = arr[i].name + arr[i].address.city
+    let user = document.createElement('ul');
+    user.innerText = arr[i].name + ' ' + arr[i].address.city
+    user.dataset.id = arr[i].id
+    user.className = 'user-info'
     ul.append(user)
   }
-  console.timeLog('test')
 }
-console.log('global')
-console.timeLog('test')
+
+function getUserPosts(e){
+  if(e.target.dataset.id){
+    const userId = e.target.dataset.id
+    console.log(e.target)
+    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+
+        const allTitles = data.map(title=>{
+          return title.title
+        })
+
+        allTitles.forEach(title=>{
+          let name = document.createElement('li')
+          name.innerText = title
+          name.className = 'user-title'
+          e.target.append(name)
+        })
+      })
+  }
+}
 
 // https://jsonplaceholder.typicode.com/posts?userId=3
