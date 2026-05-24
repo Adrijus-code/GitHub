03c8c02216f19575ba5b9110b73db3ad69d1e058
AdrijusC <263057910+Adrijus-code@users.noreply.github.com>
2026-05-24 16:49:39 +0300

    Updated task

--- learning_Val/promises/script.js
+++ learning_Val/promises/script.js
@@ -6,45 +6,63 @@ fetch("https://jsonplaceholder.typicode.com/users")
   .then((payload) => payload.json())
   .then((data) => {
     displayUser(data)
-    const allUsers = document.querySelectorAll('.user-info')
-    allUsers.forEach(user=>{
-      user.addEventListener('click', getUserPosts)
-    })
   });
 
 
 
-function displayUser(arr){
-  for(let i = 0; i < arr.length; i++){
+function displayUser(arr) {
+  for (let i = 0; i < arr.length; i++) {
     let user = document.createElement('ul');
+    user.classList.add('hide')
+    let button = document.createElement('button')
+    button.innerText = 'Show posts'
     user.innerText = arr[i].name + ' ' + arr[i].address.city
-    user.dataset.id = arr[i].id
-    user.className = 'user-info'
+    // user.dataset.id = arr[i].id
+    user.classList.add('user-info')
+
+    button.addEventListener('mouseover', () => {getUserPosts(arr[i].id, user)})
+    button.addEventListener('click', () => {showUserPosts(user)
+      if(button.innerText === 'Show posts'){
+        button.innerText = 'Hide posts'
+      }else{
+        button.innerText = 'Show posts'
+      }
+    })
+    user.append(button)
     ul.append(user)
   }
 }
 
-function getUserPosts(e){
-  if(e.target.dataset.id){
-    const userId = e.target.dataset.id
-    console.log(e.target)
-    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
+function getUserPosts(id, node) {
+  console.log([node.children.length])
+  console.log(node)
+  if (node.children.length === 1) {
+    if (id) {
+      fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
 
-        const allTitles = data.map(title=>{
-          return title.title
-        })
+          const allTitles = data.map(title => {
+            return title.title
+          })
 
-        allTitles.forEach(title=>{
-          let name = document.createElement('li')
-          name.innerText = title
-          name.className = 'user-title'
-          e.target.append(name)
+          allTitles.forEach(title => {
+            let name = document.createElement('li')
+            name.innerText = title
+            name.className = 'user-title'
+            node.append(name)
+          })
         })
-      })
+    }
   }
 }
 
+function showUserPosts(node){
+  if (node.classList.contains('hide')) {
+      node.classList.remove('hide')
+    } else {
+      node.classList.add('hide')
+    }
+}
 // https://jsonplaceholder.typicode.com/posts?userId=3
